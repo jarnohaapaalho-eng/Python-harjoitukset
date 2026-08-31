e479ee237e5e4cf7dcdd6423afaeb0acda4fbad5
Jarno <jarnohaapaalho@gmail.com>
2026-08-31 18:19:41 +0300

    This is an exercise how to make a commit and push

---
+++ exercises_temp.py
@@ -0,0 +1,5 @@
+# This .py file is intended as temporary exercises done during the class or otherwise
+
+nimi = input("What is your name?")
+
+print ("Hello " + nimi)
--- readme.md
+++ readme.md
@@ -4,3 +4,27 @@
 
 
 ## Moduuli 1 
+
+
+
+## Moduuli 2
+
+
+
+## Moduuli 3
+
+
+
+## Moduuli 4
+
+
+
+## Moduuli 5
+
+
+
+## Moduuli 6
+
+
+
+## Moduuli 7
