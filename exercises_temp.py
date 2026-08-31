1315bc66ae56b5a5962ec273e2146e97645653d8
Jarno <jarnohaapaalho@gmail.com>
2026-08-31 19:46:10 +0300

    Yet another test push

--- exercises_temp.py
+++ exercises_temp.py
@@ -1,5 +1,40 @@
 # This .py file is intended as temporary exercises done during the class or otherwise
 
-nimi = input("What is your name?")
+#libraries
+import math
+import random
 
-print ("Hello " + nimi)
+#variables
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+new_age = age + 10
+
+number_1 = 3
+number_2 = 4
+
+height = float(input("how tall are you? "))
+weight = float(input("how much do you weight? "))
+bmi = weight / (height / 100) **2
+
+# Output
+
+print ("Hello, my name is " + name + " and in 10 years I'll be " + str(new_age) + " years old")
+
+print (f"Your BMI is: {bmi:.2f}")
+
+print ("Don't mind the maths below.")
+
+#ez maths
+print(number_1 + number_2)
+print(number_1 - number_2)
+print(number_1 * number_2)
+print(number_1 / number_2)
+
+# how many times the number can go in to the other (i.e 3 goes twice to 6, but only once to 5)
+print(number_1 // number_2)
+
+# modulo / jakojäännös
+print(number_1 % number_2)
+
+# exponentials
+print(number_1 ** number_2)
---
+++ variables.py
@@ -0,0 +1,11 @@
+# Example variables
+
+# strings, str, remember quotation marks
+name = "Jarno"
+
+# integer, int (whole number)
+age = 33
+
+# float (decimal number)
+age = 33.3
+
